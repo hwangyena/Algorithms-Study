ea6adfb3e073dcaf58f86ba59e473011036acc5a
hwangyena <center7332@naver.com>
2020-11-17 00:33:53 +0900

    pyramid

---
+++ src/basicAlgorithm/Pyramid.java
@@ -0,0 +1,30 @@
+package basicAlgorithm;
+
+public class Pyramid {
+	static void spira(int n) {
+		for(int i=1; i<=n; i++) { //피라미드 층 수
+			for(int j=1; j<=n+i-1; j++) {
+				if(j>=(n-(i-1))) {
+					System.out.print("*");
+				}
+				else System.out.print(" ");
+			}System.out.println("");
+		}
+	}
+
+	static void npira(int n) {
+		for(int i=1; i<=n; i++) { //피라미드 층 수
+			for(int j=1; j<=n+i-1; j++) {
+				if(j>=(n-(i-1))) {
+					System.out.print(i);
+				}
+				else System.out.print(" ");
+			}System.out.println("");
+		}
+	}
+	public static void main(String[] args) {
+		spira(5);	//별모양 피라미드
+		npira(4);	//숫자 피라미드
+	}
+
+}
