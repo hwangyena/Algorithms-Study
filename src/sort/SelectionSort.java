c9cc14b69ff7eba94e38fb3e68a304cfea2e3cdc
hwangyena <center7332@naver.com>
2020-12-29 22:38:51 +0900

    selectionsort

---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,49 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SelectionSort {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void selectionSort(int[] array, int n) {
+		for(int i=0; i < n-1; i++) {
+			int min = i;
+			for(int j=i+1; j<n; j++) {
+				if(array[j] < array[min]) min = j;
+			}
+			swap(array, min, i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("[선택 정렬]");
+			System.out.print("요솟수 : ");
+			int num = Integer.parseInt(br.readLine());
+			int sort_arr[] = new int[num];
+			
+			for(int i=0; i<num; i++) {
+				System.out.print("arr["+i+"] : ");
+				sort_arr[i] = Integer.parseInt(br.readLine());
+			}
+			
+			selectionSort(sort_arr, num);
+			System.out.println("오름차순으로 정렬했습니다.");
+			
+			for(int i=0; i<num; i++) {
+				System.out.println("arr["+i+"] : "+sort_arr[i]);
+			}
+			
+			br.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
