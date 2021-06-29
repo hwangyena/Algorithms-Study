a39bcd2e9060bc57ff26b2360bb0af4071b3c194
hwangyena <center7332@naver.com>
2021-06-29 20:34:23 +0900

    lv1

--- programmers_LV01/rectangleStar.js
+++ programmers_LV01/rectangleStar.js
@@ -3,7 +3,7 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
 
-    //my answer
+    //My answer
     let str = '';
     for(let i=0; i<b; i++){
         for(let j=0; j<a; j++){
