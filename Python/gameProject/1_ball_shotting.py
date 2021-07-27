4f25744fd6478d6b774cce61568406e3aa2f2d16
hwangyena <center7332@naver.com>
2021-07-27 16:03:23 +0900

    4. fps

--- Python/gameProject/1_ball_shotting.py
+++ Python/gameProject/1_ball_shotting.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 #화면 타이틀 설정
 pygame.display.set_caption("Game Project") #게임 명
 
+#FPS
+clock = pygame.time.Clock()
+
 #배경 이미지 불러오기
 background = pygame.image.load("D:/Algorithms/Algorithms-Study/Python/gameProject/background.png") #480*640
 
@@ -22,23 +25,29 @@ character_y = screen_height-character_height  #캐릭터 세로 위치
 
 #이동 좌표
 to_x = 0; to_y=0
+#이동 속도
+character_speed = 0.5
 
 # event loop
 running = True #게임이 진행중인지
 while running:
+    frame = clock.tick(100) #게임화면 초당 프레임 수
+
+    #print("fps: "+str(clock.get_fps()))
+
     for event in pygame.event.get():  #이벤트가 발생했는지?
         if event.type == pygame.QUIT: #창을 닫으면(우측 상단 X버튼) 
             running = False            #종료
 
         if event.type == pygame.KEYDOWN: #키가 눌렸는지 확인
             if event.key == pygame.K_LEFT: #왼쪽
-                to_x -= 1
+                to_x -= character_speed
             if event.key == pygame.K_RIGHT: #오른쪽
-                to_x += 1
+                to_x += character_speed
             if event.key == pygame.K_UP: #위
-                to_y -= 1
+                to_y -= character_speed
             if event.key == pygame.K_DOWN: #아래
-                to_y += 1
+                to_y += character_speed
 
         if event.type == pygame.KEYUP: #방향키를 떼면
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
@@ -46,8 +55,8 @@ while running:
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN: 
                 to_y = 0
 
-    character_x += to_x
-    character_y += to_y
+    character_x += to_x * frame
+    character_y += to_y * frame
 
     #화면밖을 벗어나지 않도록
     #가로 경계 처리
